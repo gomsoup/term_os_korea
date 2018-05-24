68d28f1e8b6000773c66208230c93965456f3ed1
Dohyeon An <gomm__@pusan.ac.kr>
2018-05-24 23:26:14 +0900

    new: now we define all scheduling algorithms in schedule.h, cpp

---
+++ schedule.cpp
@@ -0,0 +1,35 @@
+#include "schedule.h"
+
+void schedule::scheduleStart(ready_queue &r, waiting_queue &q){
+	FCFSStart();
+	RRStart();
+	nonPreemptivePriorityStart();
+	preemptivePriorityStart();
+	nonPreemptiveSJFStart();
+	preemptiveSJFStart();
+}
+
+
+void nonPreemptiveSJFStart() {
+
+}
+
+void preemptiveSJFStart() {
+
+}
+
+void nonPreemptivePriorityStart() {
+
+}
+
+void preemptivePriorityStart() {
+
+}
+
+void FCFSStart() {
+
+}
+
+void RRStart() {
+
+}
